2e87e3d0f4dedcf8ad0fd73b3d02e95b2f222a07
neuhaus <neuhaus@rundp.de>
2025-12-01 11:55:57 +0100

    Day one complete

--- src/main/java/DayOne.java
+++ src/main/java/DayOne.java
@@ -1,15 +1,37 @@
+import java.util.List;
+
 public class DayOne {
 
-    public void firstPuzzle() {
+    public int firstPuzzle(List<String> rotations) {
         int dial = 50;
+        int countZeros = 0;
 
-        if (dial > 99) {
-            dial = 0;
+        for (String rotation : rotations) {
+            String direction = rotation.substring(0, 1);
+            int steps = Integer.parseInt(rotation.substring(1));
+
+            switch (direction) {
+                case "L":
+                    dial -= steps;
+                    break;
+                case "R":
+                    dial += steps;
+                    break;
+            }
+
+            while (dial < 0) {
+                dial += 100;
+            }
+
+            while (dial > 99) {
+                dial -= 100;
+            }
+
+            if (dial == 0) {
+                countZeros++;
+            }
         }
 
-        if (dial < 0) {
-            dial = 99;
-        }
-
+        return countZeros;
     }
 }
--- src/test/java/DayOneTest.java
+++ src/test/java/DayOneTest.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DayOneTest {
 
     @Test
-    void firstPuzzle() {
+    void firstPuzzle() throws IOException {
+        DayOne dayOne = new DayOne();
+        Path path = Paths.get("src/main/resources/Input-DayOne-PuzzleOne.txt");
+
+        List<String> rotations = Files.readAllLines(path);
+
+        assertEquals(-1, dayOne.firstPuzzle(rotations));
     }
 }
