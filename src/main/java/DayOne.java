aca3e3d849d9d7fb763c5dfd3f5dec92b850cb22
neuhaus <neuhaus@outlook.de>
2025-12-01 10:50:46 +0100

    Add DayOne class, test structure, and JUnit dependency

---
+++ src/main/java/DayOne.java
@@ -0,0 +1,15 @@
+public class DayOne {
+
+    public void firstPuzzle() {
+        int dial = 50;
+
+        if (dial > 99) {
+            dial = 0;
+        }
+
+        if (dial < 0) {
+            dial = 99;
+        }
+
+    }
+}
